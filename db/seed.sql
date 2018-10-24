2fd322d0415056efed8c9ba1340e59c3092ec344
Janell Anderson <andersonjanell@gmail.com>
2018-10-24 19:55:25 -0400

    Working off of a pull request made by a colleague helping me with the comments section.

--- db/seed.sql
+++ db/seed.sql
@@ -1,21 +1,21 @@
 \c blog_db
 
 
--- INSERT INTO users (username, email, password)
--- VALUES
--- ('applesauce', 'iloveapplesauce@email.com', 'applesauce'),
--- ('pumpkinspice', 'ilovepumpkinspice@email.com', 'punpkinspice');
+INSERT INTO users (username, email, password)
+VALUES
+('applesauce', 'iloveapplesauce@email.com', 'applesauce'),
+('pumpkinspice', 'ilovepumpkinspice@email.com', 'punpkinspice');
 
--- INSERT INTO posts (title, text, img_url, user_id)
--- VALUES
--- ('First Blog', 'Hi this is my first post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 1),
--- ('Second Blog', 'Hi this is my second post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 1),
--- ('Third Blog', 'Hi this is my third post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 2),
--- ('Fourth Blog', 'Hi this is my fourth post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 2);
+INSERT INTO posts (title, text, img_url, user_id)
+VALUES
+('First Blog', 'Hi this is my first post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 1),
+('Second Blog', 'Hi this is my second post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 1),
+('Third Blog', 'Hi this is my third post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 2),
+('Fourth Blog', 'Hi this is my fourth post. Welcome', 'https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif', 2);
 
--- INSERT INTO comments (text, post_id, user_id)
--- VALUES
--- ('First Comment First Blog', 1, 2),
--- ('Second Comment First Blog', 1, 2),
--- ('First Comment Second Blog', 2, 1),
--- ('Second Comment Second Blog', 2, 1);
+INSERT INTO comments (text, post_id, user_id)
+VALUES
+('First Comment First Blog', 1, 2),
+('Second Comment First Blog', 1, 2),
+('First Comment Second Blog', 2, 1),
+('Second Comment Second Blog', 2, 1);
